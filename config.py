0d75df427a34b40780d1c23c5556789ea2e041ff
Jessé Chagas <113779466+JesseChagas@users.noreply.github.com>
2023-06-24 18:57:38 -0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-host= ' '
-username = ' '
-password = ' '
-bot_token = ' '
-
-planilha_A = 'Planilhas/Relatorio.xls'
-planilha_P = 'Planilhas/professores.xls'
-planilha_AL = 'Planilhas/algoritmos.xls'
