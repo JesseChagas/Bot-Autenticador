9c92be6c61f0bf0e41bb58d22bfcc0368b1b47ad
Jessé Chagas <113779466+JesseChagas@users.noreply.github.com>
2023-06-24 18:57:59 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+host= ' '
+username = ' '
+password = ' '
+bot_token = ' '
+
+planilha_A = 'Planilhas/Relatorio.xls'
+planilha_P = 'Planilhas/professores.xls'
+planilha_AL = 'Planilhas/algoritmos.xls'
